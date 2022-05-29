60eaa11a1cc14d6d96f572902e5b6a35a747a908
Samridhi Kapoor <83203229+samridhikapoor@users.noreply.github.com>
2022-05-29 22:12:37 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,5 +1,5 @@
 from keras.applications.mobilenet_v2 import preprocess_input
-from keras.preprocessing.image import img_to_array
+from keras.utils import img_to_array
 from keras.models import load_model
 from imutils.video import VideoStream
 import numpy as np
